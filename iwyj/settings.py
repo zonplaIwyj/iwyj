5419bbd270f73a901d8e3f73b3ad636cdb518523
zonplaIwyj <iaou@vip.qq.com>
2018-12-05 23:06:36 -0800

    创建项目

--- iwyj/settings.py
+++ iwyj/settings.py
@@ -77,8 +77,10 @@ WSGI_APPLICATION = 'iwyj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mywyjiao',
+        'USER':'root',
+        'PASSWORD':'313450010',
     }
 }
 
--- iwyj/urls.py
+++ iwyj/urls.py
@@ -1,4 +1,4 @@
-ccd"""iwyj URL Configuration
+"""iwyj URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^',include(app.urls,namespace=axf)),
+    url(r'^',include('app.urls',namespace='axf')),
 ]
