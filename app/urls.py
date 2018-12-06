bcd75e9242aa7324562b27065ea1fcf33337c902
zonplaIwyj <iaou@vip.qq.com>
2018-12-07 02:19:11 +0800

    首页（少部分）

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,9 @@ from django.conf.urls import url
 from app import views
 
 urlpatterns = [
-    url(r'^$',views.index),
+    url(r'^$', views.home, name='index'),
+    url(r'^home/$', views.home, name='home'),
+    url(r'^market/$', views.market, name='market'),
+    url(r'^cart/$', views.cart, name='cart'),
+    url(r'^mine/$', views.mine, name='mine'),
 ]
--- app/views.py
+++ app/views.py
@@ -2,5 +2,17 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
-    return HttpResponse('wyjiao')
+def home(request):
+    return render(request,'home/home.html')
+
+
+def market(request):
+    return render(request, 'market/market.html')
+
+
+def cart(request):
+    return render(request, 'cart/cart.html')
+
+
+def mine(request):
+    return render(request, 'mine/mine.html')
